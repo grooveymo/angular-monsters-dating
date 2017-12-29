97a99fc1004a5ebe67146a0cab81edc2c503f768
mo sayed <mo.sayed@anatwine.com>
2017-12-29 20:33:39 +0000

    added tests for pipe

--- src/app/shared/pipes/capitalize-name.pipe.spec.ts
+++ src/app/shared/pipes/capitalize-name.pipe.spec.ts
@@ -1,8 +1,59 @@
 import { CapitalizeNamePipe } from './capitalize-name.pipe';
 
 describe('CapitalizeNamePipe', () => {
+
+  let pipe: CapitalizeNamePipe;
+  let result: string;
+
+  beforeEach(() => {
+    pipe = new CapitalizeNamePipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new CapitalizeNamePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should handle empty value', () => {
+    const input = '';
+    result = pipe.transform(input);
+    expect(result).toBe(input);
+  });
+
+  it('should handle null value', () => {
+    const input = null;
+    result = pipe.transform(input);
+    expect(result).toBe(input);
+  });
+
+  it('should handle undefined value', () => {
+    const input = undefined;
+    result = pipe.transform(input);
+    expect(result).toBe(input);
+  });
+
+  it('should handle lower cased values', () => {
+    const input = 'count dracula';
+    result = pipe.transform(input);
+    expect(result).toBe('Count Dracula');
+  });
+
+  it('should handle upper cased values', () => {
+    const input = 'COUNT DRACULA';
+    result = pipe.transform(input);
+    expect(result).toBe(input);
+  });
+
+  it('should handle already capitalized values', () => {
+    const input = 'Count Dracula';
+    result = pipe.transform(input);
+    expect(result).toBe('Count Dracula');
+  });
+
+  it('should handle mixed case values', () => {
+    const input = 'count Dracula';
+    result = pipe.transform(input);
+    console.log('before: ' + input + ' after: ' + result);
+    expect(result).toBe('Count Dracula');
+  });
+
 });
--- src/app/shared/pipes/capitalize-name.pipe.ts
+++ src/app/shared/pipes/capitalize-name.pipe.ts
@@ -12,6 +12,9 @@ export class CapitalizeNamePipe implements PipeTransform {
   readonly REGEX_CAPITALIZE_NAME = /\b\w/g;
 
   transform(value: any, args?: any): any {
+
+    if (!value) return value;
+
     return value.replace(this.REGEX_CAPITALIZE_NAME, first => first.toLocaleUpperCase());
   }
 
